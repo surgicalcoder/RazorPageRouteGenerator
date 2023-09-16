454dace781fe937dc6fc48dd96250ecac30761a6
SurgicalCoder <me@surgicalcoder.com>
2023-09-16 11:23:36 +0100

    Test incremental work

---
+++ GoLive.Generator.RazorPageRoute.Generator/PageRouteIncrementalGenerator.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace GoLive.Generator.RazorPageRoute.Generator;
+
+[Generator]
+public class PageRouteIncrementalGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        IncrementalValuesProvider<PageRoute> declarations = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                static (s, _) => Scanner.CanBeRazorPage(s),
+                static (ctx, _) => GetRazorDeclarations(ctx))
+            .Where(static c => c is not null)
+            .SelectMany(static (c, _) => Scanner.ConvertToRoute(c));
+
+        var configFiles = context.AdditionalTextsProvider.Where(IsConfigurationFile);
+        var controllersAndConfig = declarations.Collect().Combine(configFiles.Collect());
+        var defaultValuesCombined = context.AnalyzerConfigOptionsProvider.Combine(controllersAndConfig);
+//context.RegisterSourceOutput(context.AdditionalTextsProvider, (spc, text) => Exec4(spc, text));
+ //       context.RegisterSourceOutput(context.CompilationProvider, (spc, compilation) => Exec3(spc, compilation));
+        
+        context.RegisterSourceOutput(defaultValuesCombined, (spc, tuple) => Execute(spc, tuple));
+    }
+
+    private void Exec4(SourceProductionContext spc, AdditionalText text)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Exec3(SourceProductionContext spc, Compilation compilation)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private void Execute(SourceProductionContext context, (AnalyzerConfigOptionsProvider configProvider, (ImmutableArray<PageRoute> pageRoutes, ImmutableArray<AdditionalText> Config) Right) input)
+    {
+        _ = input.configProvider.GlobalOptions.TryGetValue("build_property.rootnamespace", out string defaultNamespace);
+        
+        var config = LoadConfig(input.Right.Config, defaultNamespace);
+        
+        var source = Generate(config, input.Right.pageRoutes);
+
+        if (!string.IsNullOrWhiteSpace(source))
+        {
+            context.AddSource("PageRoutes.g.cs", source.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+
+
+    private string Generate(Settings config, IEnumerable<PageRoute> context)
+    {
+        var source = new SourceStringBuilder();
+
+        if (config.OutputLastCreatedTime)
+        {
+            source.AppendLine($"// This file was generated on {DateTime.Now:R}");
+        }
+
+        source.AppendLine("using System;");
+        source.AppendLine("using System.Net.Http;");
+        source.AppendLine("using System.Threading.Tasks;");
+        source.AppendLine("using System.Net.Http.Json;");
+        source.AppendLine("using System.Collections.Generic;");
+
+        if (config.OutputExtensionMethod)
+        {
+            source.AppendLine("using Microsoft.AspNetCore.Components;");
+        }
+
+        source.AppendLine($"namespace {config.Namespace}");
+        source.AppendOpenCurlyBracketLine();
+        source.AppendLine($"public static class {config.ClassName}");
+        source.AppendOpenCurlyBracketLine();
+
+        if (!context.Any())
+        {
+            return null;
+        }
+
+        foreach (var pageRoute in context)
+        {
+            var routeTemplate = TemplateParser.ParseTemplate(pageRoute.Route);
+
+            var SlugName = Slug.Create(pageRoute.Route.Length > 1 ? string.Join(".", routeTemplate.Segments.Where(f => !f.IsParameter).Select(f => f.Value)) : "Home");
+
+            var routeSegments = routeTemplate.Segments.Where(e => e.IsParameter).Select(delegate(TemplateSegment segment)
+            {
+                var constraint = segment.Constraints.Any() ? segment.Constraints.FirstOrDefault().GetConstraintType() : null;
+
+                if (constraint == null)
+                {
+                    return $"string {segment.Value}";
+                }
+
+                return segment.IsOptional ? $"{constraint.FullName}? {segment.Value}" : $"{constraint.FullName} {segment.Value}";
+            }).ToList();
+
+            if (pageRoute.QueryString is { Count: > 0 })
+            {
+                routeSegments.AddRange(pageRoute.QueryString.Select(prqp => $"{prqp.Type.ToDisplayString()}{(prqp.Type.IsReferenceType ? "?" : "")} {prqp.Name} = default"));
+            }
+
+            var parameterString = string.Join(", ", routeSegments);
+
+            OutputRouteStringMethod(source, SlugName, parameterString, routeTemplate, pageRoute);
+
+            if (config.OutputExtensionMethod)
+            {
+                OutputRouteExtensionMethod(source, SlugName, parameterString, routeTemplate, pageRoute);
+            }
+        }
+
+        source.AppendCloseCurlyBracketLine();
+        source.AppendCloseCurlyBracketLine();
+
+        var outp = source.ToString();
+
+        return outp;
+    }
+
+    private void OutputRouteStringMethod(SourceStringBuilder source, string SlugName, string parameterString, RouteTemplate routeTemplate, PageRoute pageRoute)
+    {
+        source.AppendLine($"public static string {SlugName} ({parameterString})");
+        source.AppendOpenCurlyBracketLine();
+
+        if (routeTemplate.Segments.Any(e => e.IsParameter))
+        {
+            source.AppendIndent();
+            source.Append("string url = $\"", false);
+
+            foreach (var seg in routeTemplate.Segments)
+            {
+                if (seg.IsParameter)
+                {
+                    source.Append($"/{{{seg.Value}.ToString()}}", false);
+                }
+                else
+                {
+                    source.Append($"/{seg.Value}", false);
+                }
+            }
+
+            source.Append("\";\n", false);
+        }
+        else
+        {
+            source.AppendLine($"string url = \"{pageRoute.Route}\";");
+        }
+
+        if (pageRoute.QueryString is { Count: > 0 })
+        {
+            source.AppendLine("Dictionary<string, string> queryString=new();");
+
+            foreach (var pageRouteQuerystringParameter in pageRoute.QueryString)
+            {
+                if (pageRouteQuerystringParameter.Type.ToDisplayString(symbolDisplayFormat) == "System.String")
+                {
+                    source.AppendLine($"if (!string.IsNullOrWhiteSpace({pageRouteQuerystringParameter.Name})) ");
+                }
+                else
+                {
+                    source.AppendLine($"if ({pageRouteQuerystringParameter.Name} != default) ");
+                }
+
+                source.AppendOpenCurlyBracketLine();
+                source.AppendLine($"queryString.Add(\"{pageRouteQuerystringParameter.Name}\", {pageRouteQuerystringParameter.Name}.ToString());");
+                source.AppendCloseCurlyBracketLine();
+            }
+
+            source.AppendLine("");
+            source.AppendLine("url = Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString(url, queryString);");
+        }
+
+        source.AppendLine("return url;");
+
+        source.AppendCloseCurlyBracketLine();
+    }
+
+    private void OutputRouteExtensionMethod(SourceStringBuilder source, string SlugName, string parameterString, RouteTemplate routeTemplate, PageRoute pageRoute)
+    {
+        if (string.IsNullOrWhiteSpace(parameterString))
+        {
+            source.AppendLine($"public static void {SlugName} (this NavigationManager manager, bool forceLoad = false, bool replace=false)");
+        }
+        else
+        {
+            source.AppendLine($"public static void {SlugName} (this NavigationManager manager, {parameterString}, bool forceLoad = false, bool replace=false)");
+        }
+
+        source.AppendOpenCurlyBracketLine();
+
+        if (routeTemplate.Segments.Any(e => e.IsParameter))
+        {
+            source.AppendIndent();
+            source.Append("string url = $\"", false);
+
+            foreach (var seg in routeTemplate.Segments)
+            {
+                if (seg.IsParameter)
+                {
+                    source.Append($"/{{{seg.Value}.ToString()}}", false);
+                }
+                else
+                {
+                    source.Append($"/{seg.Value}", false);
+                }
+            }
+
+            source.Append("\";\n", false);
+        }
+        else
+        {
+            source.AppendLine($"string url = \"{pageRoute.Route}\";");
+        }
+
+        if (pageRoute.QueryString is { Count: > 0 })
+        {
+            source.AppendLine("Dictionary<string, string> queryString=new();");
+
+            foreach (var pageRouteQuerystringParameter in pageRoute.QueryString)
+            {
+                if (pageRouteQuerystringParameter.Type.ToDisplayString(symbolDisplayFormat) == "System.String")
+                {
+                    source.AppendLine($"if (!string.IsNullOrWhiteSpace({pageRouteQuerystringParameter.Name})) ");
+                }
+                else
+                {
+                    source.AppendLine($"if ({pageRouteQuerystringParameter.Name} != default) ");
+                }
+
+                source.AppendOpenCurlyBracketLine();
+                source.AppendLine($"queryString.Add(\"{pageRouteQuerystringParameter.Name}\", {pageRouteQuerystringParameter.Name}.ToString());");
+                source.AppendCloseCurlyBracketLine();
+            }
+
+            source.AppendLine("");
+            source.AppendLine("url = Microsoft.AspNetCore.WebUtilities.QueryHelpers.AddQueryString(url, queryString);");
+        }
+
+        source.AppendLine("manager.NavigateTo(url, forceLoad, replace);");
+        source.AppendCloseCurlyBracketLine();
+    }
+    
+    private static Settings LoadConfig(ImmutableArray<AdditionalText> configFiles, string defaultNamespace)
+    {
+        var configFilePath = configFiles.FirstOrDefault();
+
+        if (configFilePath == null)
+        {
+            var settings = new Settings();
+            settings.Namespace = defaultNamespace;
+            settings.ClassName = "PageRoutes";
+            return settings;
+        }
+
+        var jsonString = File.ReadAllText(configFilePath.Path);
+        var config = JsonSerializer.Deserialize<Settings>(jsonString);
+        var configFileDirectory = Path.GetDirectoryName(configFilePath.Path);
+
+        if (string.IsNullOrEmpty(config.Namespace))
+        {
+            config.Namespace = defaultNamespace;
+        }
+
+        if (!string.IsNullOrWhiteSpace(config.OutputToFile))
+        {
+            var fullPath = Path.Combine(configFileDirectory, config.OutputToFile);
+            config.OutputToFile = Path.GetFullPath(fullPath);
+        }
+
+        return config;
+    }
+    
+    private static INamedTypeSymbol GetRazorDeclarations(GeneratorSyntaxContext context)
+    {
+        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+        var symbol = (INamedTypeSymbol)context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax);
+
+        return symbol is not null && Scanner.IsRoutableRazorComponent(symbol) ? symbol : null;
+    }
+
+    private static bool IsConfigurationFile(AdditionalText text)
+    {
+        return text.Path.EndsWith("RazorPageRoutes.json");
+    }
+
+    private static readonly SymbolDisplayFormat symbolDisplayFormat = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces);
+}
--- GoLive.Generator.RazorPageRoute.Generator/Scanner.cs
+++ GoLive.Generator.RazorPageRoute.Generator/Scanner.cs
@@ -1,74 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
 
-namespace GoLive.Generator.RazorPageRoute.Generator
+namespace GoLive.Generator.RazorPageRoute.Generator;
+
+public static class Scanner
 {
-    public static class Scanner
+    private static SymbolDisplayFormat symbolDisplayFormat = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces);
+    
+    public static bool CanBeRazorPage(SyntaxNode node)
+        => node is ClassDeclarationSyntax c
+           && !c.Modifiers.Any(m => m.IsKind(SyntaxKind.AbstractKeyword))
+           && c.BaseList?.Types.Count > 0;
+
+    
+    public static bool IsRoutableRazorComponent(INamedTypeSymbol classDeclaration) => InheritsFrom(classDeclaration, "Microsoft.AspNetCore.Components.ComponentBase");
+    
+    public static IEnumerable<PageRoute> ConvertToRoute(INamedTypeSymbol classSymbol)
     {
-        public static IEnumerable<PageRoute> ScanForPageRoutes(SemanticModel semantic)
+        var attributes = FindAttributes(classSymbol, a => a.ToString() == "Microsoft.AspNetCore.Components.RouteAttribute");
+
+        var queryStringParams = classSymbol.GetMembers().OfType<IPropertySymbol>();
+        var querystringParameters = (from qsParam in queryStringParams let qsAttr = FindAttribute(qsParam, a => a.ToString() == "Microsoft.AspNetCore.Components.SupplyParameterFromQueryAttribute") where qsAttr != null select new PageRouteQuerystringParameter(qsAttr?.ConstructorArguments.FirstOrDefault().Value?.ToString() ?? qsParam.Name, qsParam.Type)).ToList();
+
+        foreach (var attributeData in attributes)
         {
-            var baseClass = semantic.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Components.ComponentBase");
+            var route = attributeData?.ConstructorArguments.FirstOrDefault().Value?.ToString() ?? string.Empty;
 
-            if (baseClass == null)
+            yield return new PageRoute(classSymbol.Name, route, querystringParameters);
+        }
+    }
+    
+    
+    private static AttributeData? FindAttribute(ISymbol symbol, Func<INamedTypeSymbol, bool> selectAttribute)
+        => symbol
+            .GetAttributes()
+            .LastOrDefault(a => a?.AttributeClass != null && selectAttribute(a.AttributeClass));
+    
+    private static IEnumerable<AttributeData> FindAttributes(ISymbol symbol, Func<INamedTypeSymbol, bool> selectAttribute) => symbol.GetAttributes().Where(a => a?.AttributeClass != null && selectAttribute(a.AttributeClass));
+
+    
+    private static bool InheritsFrom(INamedTypeSymbol classDeclaration, string qualifiedBaseTypeName)
+    {
+        var currentDeclared = classDeclaration;
+
+        while (currentDeclared.BaseType != null)
+        {
+            var currentBaseType = currentDeclared.BaseType;
+            if (string.Equals(currentBaseType.ToDisplayString(symbolDisplayFormat), qualifiedBaseTypeName, StringComparison.Ordinal))
             {
-                yield break;
+                return true;
             }
 
-            var allNodes = semantic.SyntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>();
-
-            foreach (var node in allNodes)
-            {
-                if (semantic.GetDeclaredSymbol(node) is INamedTypeSymbol classSymbol && InheritsFrom(classSymbol, baseClass))
-                {
-                    var res = ToRoute(classSymbol);
-
-                    foreach (var pageRoute in res)
-                    {
-                        yield return pageRoute;
-                    }
-                }
-            }
+            currentDeclared = currentBaseType;
         }
 
-        private static IEnumerable<PageRoute> ToRoute(INamedTypeSymbol classSymbol)
-        {
-            var attributes = FindAttributes(classSymbol, a => a.ToString() == "Microsoft.AspNetCore.Components.RouteAttribute");
-
-            var queryStringParams = classSymbol.GetMembers().OfType<IPropertySymbol>();
-            var querystringParameters = (from qsParam in queryStringParams let qsAttr = FindAttribute(qsParam, a => a.ToString() == "Microsoft.AspNetCore.Components.SupplyParameterFromQueryAttribute") where qsAttr != null select new PageRouteQuerystringParameter(qsAttr?.ConstructorArguments.FirstOrDefault().Value?.ToString() ?? qsParam.Name, qsParam.Type)).ToList();
-
-            foreach (var attributeData in attributes)
-            {
-                var route = attributeData?.ConstructorArguments.FirstOrDefault().Value?.ToString() ?? string.Empty;
-
-                yield return new PageRoute(classSymbol.Name, route, querystringParameters);
-            }
-        }
-
-        private static IEnumerable<AttributeData> FindAttributes(ISymbol symbol, Func<INamedTypeSymbol, bool> selectAttribute) => symbol.GetAttributes().Where(a => a?.AttributeClass != null && selectAttribute(a.AttributeClass));
-
-        private static AttributeData? FindAttribute(ISymbol symbol, Func<INamedTypeSymbol, bool> selectAttribute) => symbol.GetAttributes().LastOrDefault(a => a?.AttributeClass != null && selectAttribute(a.AttributeClass));
-
-        private static bool InheritsFrom(INamedTypeSymbol classDeclaration, INamedTypeSymbol targetBaseType)
-        {
-            var currentDeclared = classDeclaration;
-
-            while (currentDeclared.BaseType != null)
-            {
-                var currentBaseType = currentDeclared.BaseType;
-
-                if (currentBaseType.Equals(targetBaseType, SymbolEqualityComparer.Default))
-                {
-                    return true;
-                }
-
-                currentDeclared = currentDeclared.BaseType;
-            }
-
-            return false;
-        }
+        return false;
     }
 }

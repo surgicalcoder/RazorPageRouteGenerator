ca67a7345d4da769e29c751d2e97caacaf7f8b19
SurgicalCoder <me@surgicalcoder.com>
2024-11-23 00:42:05 +0000

    Working invokables bit

--- GoLive.Generator.RazorPageRoute.Generator/Scanner.cs
+++ GoLive.Generator.RazorPageRoute.Generator/Scanner.cs
@@ -10,60 +10,56 @@ public static class Scanner
 {
     private static readonly string componentBaseTypeName = "Microsoft.AspNetCore.Components.ComponentBase";
     public const string jsInvokableAttribute = "Microsoft.JSInterop.JSInvokableAttribute";
-            
-        public static IEnumerable<(string MethodName, string InvokableName)> ScanForInvokables(AssemblyDefinition input)
+
+    public static IEnumerable<(string MethodName, string InvokableName)> ScanForInvokables(AssemblyDefinition input)
+    {
+        var types = input.MainModule.Types
+            .Where(t => t.BaseType != null && t is { IsClass: true, IsAbstract: false } &&
+                        IsSubclassOf(t, componentBaseTypeName))
+            .ToList();
+
+        foreach (var type in types)
         {
-            var baseClass = input.MainModule.Types.FirstOrDefault(t => t.FullName == componentBaseTypeName);
-
-            if (baseClass == null)
+            if (IsSubclassOf(type, componentBaseTypeName))
             {
-                yield break;
-            }
-            
-            var types = input.MainModule.Types.Where(t => t.BaseType != null && t.IsClass && !t.IsAbstract);
-            
-            foreach (var type in types)
-            {
-                if (IsSubclassOf(type, componentBaseTypeName))
+                foreach (var method in type.Methods)
                 {
-                    foreach (var method in type.Methods)
+                    // Check if the method has the attribute [JSInvokable(string)]
+                    var jsInvokableAttr = method.CustomAttributes.FirstOrDefault(attr =>
+                        attr.AttributeType.FullName == jsInvokableAttribute &&
+                        attr.ConstructorArguments.Count > 0 &&
+                        attr.ConstructorArguments[0].Value is string);
+
+                    if (jsInvokableAttr != null)
                     {
-                        // Check if the method has the attribute [JSInvokable(string)]
-                        var jsInvokableAttr = method.CustomAttributes.FirstOrDefault(attr =>
-                            attr.AttributeType.FullName == jsInvokableAttribute &&
-                            attr.ConstructorArguments.Count > 0 &&
-                            attr.ConstructorArguments[0].Value is string);
-        
-                        if (jsInvokableAttr != null)
-                        {
-                            // Extract method name
-                            string methodName = method.Name;
+                        // Extract method name
+                        string methodName = method.Name;
 
-                            // Extract identifier in the attribute
-                            string identifier = jsInvokableAttr.ConstructorArguments[0].Value.ToString();
+                        // Extract identifier in the attribute
+                        string identifier = jsInvokableAttr.ConstructorArguments[0].Value.ToString();
 
-                            yield return ($"{type.FullName}.{method.Name}", identifier);
-                        }
+                        yield return ($"{type.FullName}.{method.Name}", identifier);
                     }
                 }
             }
         }
-        
-        
+    }
+
 
     public static string GetDllPathFromProject(string projectPath, out DefaultAssemblyResolver assemblyResolver)
     {
-        var debugPath = getHighestFolderVersion(Path.Combine(projectPath, "bin", "Debug"));;
+        var debugPath = getHighestFolderVersion(Path.Combine(projectPath, "bin", "Debug"));
+        ;
         var objDebugPath = getHighestFolderVersion(Path.Combine(projectPath, "obj", "Debug"));
         var refIntPath = Path.Combine(objDebugPath, "refInt");
-            
+
         var dllFile = Directory.GetFiles(refIntPath, "*.dll").FirstOrDefault();
 
         if (dllFile == null)
         {
             throw new FileNotFoundException("No DLL file found in refInt directory.");
         }
-            
+
         //var defaultNetPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "dotnet", "shared", "Microsoft.AspNetCore.App", "9.0.0");
         assemblyResolver = new DefaultAssemblyResolver();
         assemblyResolver.AddSearchDirectory(refIntPath);
@@ -72,11 +68,12 @@ public static class Scanner
 
         return dllFile;
     }
-    
+
     public static IEnumerable<PageRoute> ScanForPageRoutesIncremental(AssemblyDefinition input)
     {
         var types = input.MainModule.Types
-            .Where(t => t.BaseType != null && t is { IsClass: true, IsAbstract: false } && IsSubclassOf(t, componentBaseTypeName))
+            .Where(t => t.BaseType != null && t is { IsClass: true, IsAbstract: false } &&
+                        IsSubclassOf(t, componentBaseTypeName))
             .ToList();
 
         foreach (var type in types)
@@ -103,7 +100,8 @@ public static class Scanner
         var queryStringParams = input.Properties;
 
         var querystringParameters = queryStringParams
-            .Where(p => p.CustomAttributes.Any(attr => attr.AttributeType.FullName == "Microsoft.AspNetCore.Components.SupplyParameterFromQueryAttribute"))
+            .Where(p => p.CustomAttributes.Any(attr =>
+                attr.AttributeType.FullName == "Microsoft.AspNetCore.Components.SupplyParameterFromQueryAttribute"))
             .Select(p => new PageRouteQuerystringParameter(p.Name, p.PropertyType.FullName))
             .ToList();
 
@@ -132,7 +130,7 @@ public static class Scanner
     {
         return customAttributes ?? [];
     }
-    
+
     static string? getHighestFolderVersion(string inputFolder, string searchPattern = "net*")
     {
         var versionFolders = Directory.GetDirectories(inputFolder, searchPattern)
@@ -148,6 +146,4 @@ public static class Scanner
 
         return highestVersionFolder;
     }
-    
-    
 }
--- GoLive.Generator.RazorPageRoute.Tests.BlazorWebAssembly/PageRoute.cs
+++ GoLive.Generator.RazorPageRoute.Tests.BlazorWebAssembly/PageRoute.cs
@@ -1,4 +1,4 @@
-// This file was generated on Sat, 23 Nov 2024 00:36:18 GMT
+// This file was generated on Sat, 23 Nov 2024 00:40:14 GMT
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
